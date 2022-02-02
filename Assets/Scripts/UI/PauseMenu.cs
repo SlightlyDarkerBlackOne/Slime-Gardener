d2b01e192f5343fac461d9cb293c0b102f8c603b
SlightlyDarkerBlackOne <42848293+SlightlyDarkerBlackOne@users.noreply.github.com>
2022-02-02 10:52:38 +0100

    Pause Menu
    Added pause menu with ESC key and UI button to pause/unpause

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,26 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject pauseMenu;
-    bool paused;
-    // Start is called before the first frame update
-    void Start() {
-        paused = false;
-    }
+    public GameObject pauseMenuUI;
+    public static bool GameIsPaused;
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKey(KeyCode.Escape) && (paused == false)) {
-            pauseMenu.SetActive(true);
-            Time.timeScale = 0;
-            //paused = true;
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            HandlePauseResume();
         }
-        if (Input.GetKey(KeyCode.Escape) && (paused == true)) {
-            pauseMenu.SetActive(false);
-            Time.timeScale = 1;
-            paused = false;
+    }
+
+    public void HandlePauseResume() {
+        if (GameIsPaused) {
+            Resume();
+        } else {
+            Pause();
         }
+    }
+
+    private void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    private void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
     }
 }
